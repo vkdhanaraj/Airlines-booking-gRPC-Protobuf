14327824e373731c495340a7883b245193e23ae8
vkdhanaraj <vk.dhanaraj@gmail.com>
2020-07-25 23:58:19 +0530

    Add Client and Server

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# Airlines-booking-gRPC-Protobuf
+=======
+# Airlines-booking-gRPC-Protobuf
+>>>>>>> 5a947626e518a40c2a9c574c7fd9e7a87ea833b4
---
+++ client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"airlines-booking/client/booking"
+)
+
+func main() {
+
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":5000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := booking.NewTicketServiceClient(conn)
+
+	response, err := c.BookTicket(context.Background(), &booking.Passenger{Name: "Kidiyoor"})
+	if err != nil {
+		log.Fatalf("Error when calling BookTicket: %s", err)
+	}
+	log.Printf("Response from server: %d", response.SeatNumber)
+
+}

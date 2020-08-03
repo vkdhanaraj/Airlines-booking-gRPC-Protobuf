ff4430b34ac6bdd8a4cd66922ab9d7c66b698eb3
vkdhanaraj <vk.dhanaraj@gmail.com>
2020-08-03 12:05:59 +0530

    rating

--- pkg/server/rating/rating.go
+++ pkg/server/rating/rating.go
@@ -1,48 +1,45 @@
 package rating
 
 import (
-	"io"
-	"fmt"
-	"log"
 	pb "Airlines-booking-gRPC-Protobuf/genfiles/rating"
-
+	"fmt"
+	"io"
+	"log"
 )
 
 //Server struct
 type Server struct {
 }
 
-
-
-func(s *Server)RateFlights(stream pb.RatingService_RateFlightsServer) error {
+//RateFlights method
+func (s *Server) RateFlights(stream pb.RatingService_RateFlightsServer) error {
 
 	req, err := stream.Recv()
 	if err == io.EOF {
 		fmt.Print("no more data")
-	
 	}
 	if err != nil {
-			log.Fatalf( "cannot receive stream request: %v", err)
+		log.Fatalf("cannot receive stream request: %v", err)
 	}
-	fmt.Println("request received from client",req)
-	id:=req.GetFlightId()
-	rate:=req.GetRating()
-	res :=[...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id,Quality : "None"}}
-	
-	if rate<=5{
-			res =[...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id,Quality : "Bad"}}
-	}else if rate>=6 && rate<8{
-		res =[...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id,Quality : "Above Average"}}
-	}else{
-		res =[...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id,Quality : "Good"}}
+	fmt.Println("request received from client", req)
+	id := req.GetFlightId()
+	rate := req.GetRating()
+	res := [...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id, Quality: "None"}}
+
+	if rate <= 5 {
+		res = [...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id, Quality: "Bad"}}
+	} else if rate >= 6 && rate < 8 {
+		res = [...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id, Quality: "Above Average"}}
+	} else {
+		res = [...]*pb.RatingResponse{&pb.RatingResponse{FlightId: id, Quality: "Good"}}
 	}
 
 	for _, info := range res {
 		err = stream.Send(info)
 		if err != nil {
-			log.Fatalf( "cannot send stream response: %v", err)
-		}		
+			log.Fatalf("cannot send stream response: %v", err)
+		}
 	}
-		
-return nil
+
+	return nil
 }
